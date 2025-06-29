2db33c524094be856f03b1ca16d8857a3b54f50d
NelsonDev1411 <nelsongarcia141102@gmail.com>
2025-06-28 20:40:54 -0400

    Refactor code structure for improved readability and maintainability

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,54 @@
 const Records = require('./records.model');
+const fs = require('fs');
+const csv = require('csv-parse');
 
 const upload = async (req, res) => {
-    const {file} = req;
+    const { file } = req;
+    console.log(file)
+    const { path, destination, filename } = file;
+    const MAX_FILES = 2000
+    let batch = []
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
+    try {
+        fs.createReadStream(path)
+        .pipe(csv.parse({ columns: true, delimiter: ',' }))
+        .on('data', async (row) => {
+            batch.push(row);
+            if (batch.length === MAX_FILES) {
+                // insertamos los registros por lotes
+                await Records.insertMany(batch);
+                batch = [];
+            }
+        })
+        .on('end', async () => {
+            if (batch.length > 0) {
+                await Records.insertMany(batch);
+                batch = [];
+            }
+        })
+        .on('error', (err) => {
+            console.error('Error al leer el archivo:', err);
+        })
+        .on('close', async () => {
+            try {
+                console.log(file)
+                await fs.unlink(`${destination}/${filename}`, (err) => {
+                    if (err) {
+                        console.error('Error al eliminar el archivo:', err);
+                    } else {
+                        console.log('Archivo eliminado exitosamente');
+                    }
+                });
+            } catch (error) {
+                console.error('Error al eliminar el archivo:', error);
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        
+    }
 
-    return res.status(200).json({ message: 'some response' });
+    return res.status(200).json({ message: 'Files uploaded successfully' });
 };
 
 const list = async (_, res) => {
@@ -13,7 +56,6 @@ const list = async (_, res) => {
         const data = await Records
             .find({})
             .limit(10)
-            .lean();
         
         return res.status(200).json(data);
     } catch (err) {
